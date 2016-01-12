de693b0b8e7eabdc5128e2d2c1962ed53c12a506
inecon <saturnua@gmail.com>
2016-01-12 23:07:32 +0200

    reflaction v 1.1

--- Run.java
+++ Run.java
@@ -2,7 +2,10 @@ package myPack;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import static java.lang.System.out;
+import java.lang.reflect.Type;
 
 public class Run {
 	public static void main(String[] args) throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException{
@@ -18,9 +21,20 @@ public class Run {
 		 Annotation[] annotations = c.getAnnotations();
 		for (int i=0; i<allFields.length; i++) { 
 		    Class fieldType = allFields[i].getType();		  
-		    System.out.print("Name: " + allFields[i].getName()); 
-		    System.out.print("; Type: " + fieldType.getName()); 
+		    System.out.print(" " + Modifier.toString(allFields[i].getModifiers())); 
+		    System.out.print(" " + allFields[i].getName()); 
+		    System.out.print(" " + fieldType.getName()); 
 		    System.out.println(" @annotation " + annotations + " ");
-		    System.out.println();
-	}
+		}    
+		
+		System.out.println("----------------Methods------------------------");
+		Method []allMethods = c.getDeclaredMethods(); 
+		for (int j=0; j<allMethods.length; j++){
+			out.format("%s%n", allMethods[j].toGenericString());
+
+			//out.format(fmt, "ReturnType", allMethods[j].getReturnType());
+			//out.format(fmt, "GenericReturnType", allMethods[j].getGenericReturnType());
+		}
+		
+	
 }}
